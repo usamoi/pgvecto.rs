043ac79f6e5b6a1bf97ab77f3b1f4fd5d88f4838
usamoi <usamoi@outlook.com>
2024-08-19 00:19:41 +0800

    chore: update pgrx to v0.12.0
    Signed-off-by: usamoi <usamoi@outlook.com>

--- src/index/am_options.rs
+++ src/index/am_options.rs
@@ -28,7 +28,7 @@ pub struct Reloption {
 impl Reloption {
     pub const TAB: &'static [pgrx::pg_sys::relopt_parse_elt] = &[pgrx::pg_sys::relopt_parse_elt {
         optname: c"options".as_ptr(),
-        opttype: pgrx::pg_sys::relopt_type_RELOPT_TYPE_STRING,
+        opttype: pgrx::pg_sys::relopt_type::RELOPT_TYPE_STRING,
         offset: std::mem::offset_of!(Reloption, options) as i32,
     }];
     unsafe fn options(&self) -> &CStr {
